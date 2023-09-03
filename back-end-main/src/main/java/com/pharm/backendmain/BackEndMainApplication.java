1b21d05057a0c4468f1fcbef13f78c03a6d89dcb
bluefirerose <rose.a.exile@gmail.com>
2023-09-03 12:38:30 +0800

    first commit

---
+++ back-end-main/src/main/java/com/pharm/backendmain/BackEndMainApplication.java
@@ -0,0 +1,13 @@
+package com.pharm.backendmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndMainApplication.class, args);
+	}
+
+}
